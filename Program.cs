980580b9add9b65ebfbba1c97d177e51cde79944
OlegVorontsov <oleg-voroncov@mail.ru>
2023-01-13 16:11:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._6_HomeWork
+{
+    class Program
+    {
+        static void fillList (List<int> list)
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(rand.Next(100));
+            }
+        }
+
+        static void printList (List<int> list)
+        {
+            int j = 1;
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write($"{list[i]}\t");
+                if (j % 10 == 0)
+                {
+                    Console.WriteLine();
+                }
+                j++;
+            }
+        }
+
+        static void clearList(int downNumber, int upNumber, List<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if ((downNumber < list[i]) && (list[i] < upNumber))
+                {
+                    list.Remove(list[i]);
+                    i--;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<int> listOfNumbers = new List<int>();
+
+            fillList(listOfNumbers);
+            printList(listOfNumbers);
+
+            Console.Write("Введите нижний предел (от 0 до 100): ");
+            int fromNumber = int.Parse(Console.ReadLine());
+            Console.Write("Введите верхний предел (от 0 до 100): ");
+            int toNumber = int.Parse(Console.ReadLine());
+
+            clearList(fromNumber, toNumber, listOfNumbers);
+            printList(listOfNumbers);
+
+            Console.ReadLine();
+        }
+    }
+}
